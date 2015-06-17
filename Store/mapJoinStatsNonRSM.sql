70eac673d9dc171ea167c45de6d1c28e312fa103
kittypants <illyriah@gmail.com>
2015-06-16 20:50:36 -0500

    Create mapJoinStatsNonRSM.sql
    Returns map join stats for all non-RSM players.

---
+++ Store/mapJoinStatsNonRSM.sql
@@ -0,0 +1,17 @@
+-- By KittyPants/Amie Davis 6/16/15 
+-- Returns join stats by map for all non-RSM players.
+SELECT map.map_name, joinTable.joincount, departTable.departcount, joinTable.joincount - departTable.departcount as net
+FROM map
+INNER JOIN (SELECT map.map_name, count(player_connection.player_connection_id) as departcount FROM match 
+	INNER JOIN map ON match.map_id = map.map_id
+	INNER JOIN player_connection ON player_connection.match_id = match.match_id
+	INNER JOIN player ON player.player_id = player_connection.player_id
+	WHERE player.is_rsm = '0' AND connection_end_time > match_start_time AND connection_end_time <= match_end_time
+	GROUP BY map.map_name) departTable ON departTable.map_name = map.map_name
+INNER JOIN (SELECT map.map_name, count(player_connection.player_connection_id) as joincount FROM match 
+	INNER JOIN map ON match.map_id = map.map_id
+	INNER JOIN player_connection ON player_connection.match_id = match.match_id
+	INNER JOIN player ON player.player_id = player_connection.player_id
+	WHERE player.is_rsm = '0' AND connection_start_time >= match_start_time AND connection_start_time < match_end_time
+	GROUP BY map.map_name) joinTable ON joinTable.map_name = map.map_name
+ORDER BY net DESC, joincount DESC
